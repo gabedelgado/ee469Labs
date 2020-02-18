ece7bc11e4112859c212af2330e15af804c9341a
Gabriel Delgado <delgadog@purdue.edu>
2020-02-17 21:50:24 -0500

    asdf

--- lab2/apps/q5/inject1/inject1.c
+++ lab2/apps/q5/inject1/inject1.c
@@ -8,12 +8,13 @@ void main (int argc, char * argv[]){
     int numh20;
     sem_t h20;
     sem_t parent;
+    int number_molecules;
 
     numh20 = dstrtol(argv[2], NULL, 10);
     h20 = dstrtol(argv[1],NULL,10);
     parent = dstrtol(argv[3],NULL,10);
 
-    int number_molecules = numh20;
+    number_molecules = numh20;
 
 
     for(int i = 0; i < number_molecules;++i){
