04c70a4822c8b3bb56effc1bb4aee3eec48b5a84
Gabriel Delgado <delgadog@purdue.edu>
2020-02-17 21:51:46 -0500

    fuck

--- lab2/apps/q5/inject2/inject2.c
+++ lab2/apps/q5/inject2/inject2.c
@@ -8,15 +8,16 @@ void main (int argc, char * argv[]){
     int numSO4;
     sem_t sO4;
     sem_t parent;
-
+    int number_molecules;
+    int i;
     numSO4 = dstrtol(argv[2], NULL, 10);
     sO4 = dstrtol(argv[1],NULL,10);
     parent = dstrtol(argv[3],NULL,10);
 
-    int number_molecules = numSO4;
+    number_molecules = numSO4;
 
 
-    for(int i = 0; i < number_molecules;++i){
+    for(i = 0; i < number_molecules;++i){
         Printf("SO4 injected into Radeon atmosphere, PID: %d",getpid());
         sem_signal(sO4);
     }
