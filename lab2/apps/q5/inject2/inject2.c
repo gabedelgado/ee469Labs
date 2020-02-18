ca63ad2c3f3d41194364baa93acb84d103d90c08
Federico A Brandt <fbrandt@ecegrid-thin4.ecn.purdue.edu>
2020-02-17 21:17:59 -0500

    Updated injections

--- lab2/apps/q5/inject2/inject2.c
+++ lab2/apps/q5/inject2/inject2.c
@@ -13,13 +13,12 @@ void main (int argc, char * argv[]){
     sO4 = dstrtol(argv[1],NULL,10);
     parent = dstrtol(argv[3],NULL,10);
 
-    int number_molecules = numh20 % 1;
+    int number_molecules = numSO4;
 
 
     for(int i = 0; i < number_molecules;++i){
         Printf("SO4 injected into Radeon atmosphere, PID: %d",getpid());
-        sem_signal(s_sO2);
-        sem_signal(s_O2);
+        sem_signal(s_sO4);
     }
 
     sem_signal(parent)
