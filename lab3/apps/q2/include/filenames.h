6639eace404b22ace15933db956ca86363264d63
Gabriel Delgado <delgadog@purdue.edu>
2020-03-24 22:18:34 -0400

    asdfasdf

---
+++ lab3/apps/q2/include/filenames.h
@@ -0,0 +1,11 @@
+#ifndef __USERPROG__
+#define __USERPROG__
+
+#define INJECT1_TO_RUN "genS2.dlx.obj"
+#define INJECT2_TO_RUN "genCO.dlx.obj"
+
+#define REACT1_TO_RUN "react4CO.dlx.obj"
+#define REACT2_TO_RUN "reactS2.dlx.obj"
+#define REACT3_TO_RUN "reactS2O2.dlx.obj"
+
+#endif
