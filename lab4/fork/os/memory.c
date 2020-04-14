17402b7bf496dd602e6ec2e2c375916888c00b9e
Gabriel Delgado <delgadog@purdue.edu>
2020-04-14 14:44:54 -0400

    asdf

--- lab4/fork/os/memory.c
+++ lab4/fork/os/memory.c
@@ -296,7 +296,7 @@ int MemoryROPAccessHandler(PCB * pcb){
   int l1_page_num = fault_address >> MEM_L1FIELD_FIRST_BITNUM;
   int phys_page_num = (MemoryTranslateUserToSystem(pcb, pcb->currentSavedFrame[PROCESS_STACK_FAULT])) >> MEM_L1FIELD_FIRST_BITNUM;
   int pageGen;
-
+  printf("handler gets called");
   if (page_refcounters[phys_page_num] < 1){
     //Kill Process and return mem_fail
     ProcessKill();
@@ -307,6 +307,7 @@ int MemoryROPAccessHandler(PCB * pcb){
   }
   else{
     //generate new page
+    printf("generating new page for (%d)", GetPidFromAddress(pcb));
     pageGen = MemoryAllocPage();
     pcb->pagetable[l1_page_num] = MemorySetupPte(pageGen);
     //bcopy
