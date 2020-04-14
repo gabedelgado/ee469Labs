f06f17a2bcf8ff4a6556df7a4d8994ac9449da52
Gabriel Delgado <delgadog@purdue.edu>
2020-04-13 20:48:00 -0400

    asdf

--- lab4/one-level/apps/example/makeprocs/makeprocs.c
+++ lab4/one-level/apps/example/makeprocs/makeprocs.c
@@ -33,7 +33,10 @@ void main (int argc, char *argv[])
     Exit();
   }
 
-  s_bigcount_completed = sem_create(-29);
+  if ((s_bigcount_completed = sem_create(-29)) == SYNC_FAIL) {
+    Printf("makeprocs (%d): Bad sem_create\n", getpid());
+    Exit();
+  }
   // Setup the command-line arguments for the new processes.  We're going to
   // pass the handles to the semaphore as strings
   // on the command line, so we must first convert them from ints to strings.
@@ -71,8 +74,12 @@ void main (int argc, char *argv[])
   Printf("makeprocs (%d): calling bigcount process 30 times\n", getpid());
   for(z=0; z < 30; z++) {
     process_create(BIGCOUNT, s_bigcount_completed_str, NULL);
+    Printf("made bigcount #%d", z);
   }
-  sem_wait(s_bigcount_completed);
+  if (sem_wait(s_bigcount_completed) != SYNC_SUCCESS) {
+    Printf("Bad semaphore s_procs_completed (%d) in %s\n", s_procs_completed, argv[0]);
+    Exit();
+  }
 
   process_create(MAXADDRESS, s_procs_completed_str, NULL);
   sem_wait(s_procs_completed);
