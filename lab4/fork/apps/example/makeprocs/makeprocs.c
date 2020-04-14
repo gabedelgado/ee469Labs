1063f1e55491b791c8cfe32204f53008cdad883b
Gabriel Delgado <delgadog@purdue.edu>
2020-04-14 16:06:27 -0400

    asdf

--- lab4/fork/apps/example/makeprocs/makeprocs.c
+++ lab4/fork/apps/example/makeprocs/makeprocs.c
@@ -16,8 +16,8 @@ void main (int argc, char *argv[])
   }
 
   // Convert string from ascii command line argument to integer number
-  num_hello_world = dstrtol(argv[1], NULL, 10); // the "10" means base 10
-  Printf("makeprocs (%d): Creating %d hello_world processes\n", getpid(), num_hello_world);
+  // num_hello_world = dstrtol(argv[1], NULL, 10); // the "10" means base 10
+  // Printf("makeprocs (%d): Creating %d hello_world processes\n", getpid(), num_hello_world);
 
   // Create semaphore to not exit this process until all other processes 
   // have signalled that they are complete.
@@ -31,17 +31,18 @@ void main (int argc, char *argv[])
   // on the command line, so we must first convert them from ints to strings.
   ditoa(s_procs_completed, s_procs_completed_str);
 
+  Printf("------------ SPAWNING ONE HELLO WORLD PROGRAM WITH A fork() INSIDE--------------------\n");
   // Create Hello World processes
-  Printf("-------------------------------------------------------------------------------------\n");
-  Printf("makeprocs (%d): Creating %d hello world's in a row, but only one runs at a time\n", getpid(), num_hello_world);
-  for(i=0; i<num_hello_world; i++) {
-    Printf("makeprocs (%d): Creating hello world #%d\n", getpid(), i);
+  // Printf("-------------------------------------------------------------------------------------\n");
+  // Printf("makeprocs (%d): Creating %d hello world's in a row, but only one runs at a time\n", getpid(), num_hello_world);
+  // for(i=0; i<num_hello_world; i++) {
+  //   Printf("makeprocs (%d): Creating hello world #%d\n", getpid(), i);
     process_create(HELLO_WORLD, s_procs_completed_str, NULL);
     if (sem_wait(s_procs_completed) != SYNC_SUCCESS) {
       Printf("Bad semaphore s_procs_completed (%d) in %s\n", s_procs_completed, argv[0]);
       Exit();
     }
-  }
+  // }
 
   Printf("-------------------------------------------------------------------------------------\n");
   Printf("makeprocs (%d): All other processes completed, exiting main process.\n", getpid());
